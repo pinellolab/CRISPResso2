8bc9214ba0a1b628b69a49a2cf306bf559e008b3
Kendell Clement <k.clement.dev@gmail.com>
2020-07-22 22:58:51 -0400

    Update CRISPRessoWGSCORE.py

--- CRISPResso2/CRISPRessoWGSCORE.py
+++ CRISPResso2/CRISPRessoWGSCORE.py
@@ -578,12 +578,13 @@ def main():
 
                     if row['Coding_sequence'] and not pd.isnull(row['Coding_sequence']):
                         crispresso_cmd+=' -c %s' % row['Coding_sequence']
+                        
+                    crispresso_cmd=CRISPRessoShared.propagate_crispresso_options(crispresso_cmd,crispresso_options_for_wgs,args)
 
                     log_name = _jp("CRISPResso_on_"+idx) +".log"
                     print('log name is ' + log_name)
                     crispresso_cmd += " &> %s"%log_name
 
-                    crispresso_cmd=CRISPRessoShared.propagate_crispresso_options(crispresso_cmd,crispresso_options_for_wgs,args)
                     crispresso_cmds.append(crispresso_cmd)
 #                    info('Running CRISPResso:%s' % crispresso_cmd)
 #                    sb.call(crispresso_cmd,shell=True)
