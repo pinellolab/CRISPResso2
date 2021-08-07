e38d572c4444d1918c1d9f6a6793543070e8ec23
Cole Lyman <cole@colelyman.com>
2021-08-06 20:32:03 -0400

    Refactor `results/general_plots` crispresso2_info fields

--- CRISPResso2/CRISPRessoReport.py
+++ CRISPResso2/CRISPRessoReport.py
@@ -64,7 +64,7 @@ def make_report(run_data, crispresso_report_file, crispresso_folder, _ROOT):
     for fig in ['1a', '1b', '1c', '1d', '5a', '6a', '8a', '11c']:
         fig_name = 'plot_'+ fig
         if fig_name + '_root' in run_data:
-            add_fig_if_exists(fig_name, run_data[fig_name + '_root'], 'Figure ' + fig, run_data[fig_name + '_caption'], run_data[fig_name+'_data'],
+            add_fig_if_exists(fig_name, run_data['results']['general_plots'][fig_name + '_root'], 'Figure ' + fig, run_data['results']['general_plots'][fig_name + '_caption'], run_data['results']['general_plots'][fig_name+'_data'],
                 global_fig_names, fig_locs, fig_titles, fig_captions, fig_datas)
 
 
