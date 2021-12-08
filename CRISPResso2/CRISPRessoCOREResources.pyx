f2169cd1ec11f3a349900d61a084a4d9fed46fa0
Kendell Clement <k.clement.dev@gmail.com>
2021-12-08 17:13:28 -0500

    Revert "Implement algorithmic improvements for find_indels_substitutions"
    This reverts commit 13414833ef6cd5b232097f6ff0325a2b8e28b214.

--- CRISPResso2/CRISPRessoCOREResources.pyx
+++ CRISPResso2/CRISPRessoCOREResources.pyx
@@ -21,6 +21,14 @@ re_find_indels = re.compile("(-*-)")
 @cython.wraparound(False)
 def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
 
+    cdef char* sub_seq=''
+
+    cdef int st
+    cdef int en
+
+    cdef int idx_c
+    cdef int idx
+
     #ref_positions holds the indices for which positions map back to the original reference
     # for example,
     #     1 2 3 4 5 6 7 8
@@ -30,99 +38,109 @@ def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
     # ref A A T T - G G C C
     # aln A - T T T G G C C
     #     1 2 3 4-4 5 6 7 8 <ref positions. Note that the negative values/indices represent places that don't map back to the original reference
-    ref_positions = []
-    all_substitution_positions = []
-    substitution_positions = []
-    all_substitution_values = []
-    substitution_values = []
+    ref_positions=[]
+    all_substitution_positions=[]
+    substitution_positions=[]
+    all_substitution_values=[]
+    substitution_values=[]
 
-    all_deletion_positions = []
-    deletion_positions = []
-    deletion_coordinates = []
-    deletion_sizes = []
-    cdef int start_deletion = -1  # the -1 value indicates that there currently isn't a deletion
-
-    all_insertion_positions = []
-    all_insertion_left_positions = []
-    insertion_positions = []
-    insertion_coordinates = []
-    insertion_sizes = []
-    cdef int start_insertion = -1  # the -1 value indicates that there currently isn't an insertion
-
-    cdef int seq_len = len(ref_seq_al)
-    include_indx_set = set(_include_indx)
     nucSet = set(['A', 'T', 'C', 'G', 'N'])
-    cdef int idx = 0
-    cdef int idx_c
+    idx=0
     for idx_c, c in enumerate(ref_seq_al):
-        if c != '-':  # is it safe to assume c will be '-' or in nucSet? This may be marginally faster than searching the set
+        if c in nucSet:
             ref_positions.append(idx)
-            if ref_seq_al[idx_c] != read_seq_al[idx_c] and read_seq_al[idx_c] != '-' and read_seq_al[idx_c] != 'N':
+            if ref_seq_al[idx_c]!=read_seq_al[idx_c] and read_seq_al[idx_c] != '-' and read_seq_al[idx_c] != 'N':
                 all_substitution_positions.append(idx)
                 all_substitution_values.append(read_seq_al[idx_c])
                 if idx in _include_indx:
                     substitution_positions.append(idx)
                     substitution_values.append(read_seq_al[idx_c])
-            if start_insertion != -1:  # this is the end of an insertion
-                all_insertion_left_positions.append(start_insertion)
-                all_insertion_positions.append(start_insertion)
-                all_insertion_positions.append(idx)
-                if start_insertion in include_indx_set and idx in include_indx_set:
-                    insertion_coordinates.append((start_insertion, idx))
-                    insertion_positions.append(start_insertion)
-                    insertion_positions.append(idx)
-                    insertion_sizes.append(idx - start_insertion)
-                start_insertion = -1
-            idx += 1
-        else:  # the current ref position is -
-            if idx == 0:
+
+            idx+=1
+
+        else:
+            if idx==0:
                 ref_positions.append(-1)
             else:
                 ref_positions.append(-idx)
-            if idx > 0 and start_insertion == -1:  # this is the first index of an insertion
-                start_insertion = idx - 1  # check this!
-
-        if read_seq_al[idx_c] == '-' and start_deletion == -1:  # this is the first part of a deletion
-            if idx_c - 1 > 0:
-                start_deletion = ref_positions[idx_c]  # check this!
-            else:
-                start_deletion = 0
-        elif read_seq_al[idx_c] != '-' and start_deletion != -1:  # this is the end of a deletion
-            end_deletion = ref_positions[idx_c]
-            all_deletion_positions.extend(range(start_deletion, end_deletion))
-            if include_indx_set.intersection(range(start_deletion, end_deletion)):
-                deletion_positions.extend(range(start_deletion, end_deletion))
-                deletion_coordinates.append((start_deletion, end_deletion))
-                deletion_sizes.append(end_deletion - start_deletion)
-            start_deletion = -1
 
     substitution_n = len(substitution_positions)
+
+    #the remainder of positions are with reference to the original reference sequence indexes we calculated above
+    all_deletion_positions=[]
+    deletion_positions=[]
+    deletion_coordinates=[]
+    deletion_sizes=[]
+
+    all_insertion_positions=[]
+    all_insertion_left_positions=[]
+    insertion_positions=[]
+    insertion_coordinates = []
+    insertion_sizes=[]
+
+    include_indx_set = set(_include_indx)
+    for p in re_find_indels.finditer(read_seq_al):
+        st,en=p.span()
+        ref_st = 0
+        if st-1 > 0:
+          ref_st = ref_positions[st]
+        ref_en = idx-1
+        if en < len(ref_positions):
+          ref_en = ref_positions[en]
+        all_deletion_positions.extend(range(ref_st,ref_en))
+        inc_del_pos = include_indx_set.intersection(range(ref_st,ref_en))
+        if(len(inc_del_pos)>0):
+          deletion_positions.extend(range(ref_st,ref_en))
+          deletion_coordinates.append((ref_st,ref_en))
+          deletion_sizes.append(en-st)
+
     deletion_n = np.sum(deletion_sizes)
+
+    for p in re_find_indels.finditer(ref_seq_al):
+        st,en=p.span()
+        #sometimes insertions run off the end of the reference
+        if st == 0: # if insertion happened before ref
+          continue
+        if en == len(ref_seq_al): # if insertion happened after ref
+          continue
+        ref_st = ref_positions[st-1]
+        ref_en = ref_positions[en]
+
+        all_insertion_left_positions.append(ref_st)
+        all_insertion_positions.append(ref_st)
+        all_insertion_positions.append(ref_en)
+        if(ref_st in _include_indx and ref_en in _include_indx):
+          insertion_coordinates.append((ref_st,ref_en))
+          insertion_positions.append(ref_st)
+          insertion_positions.append(ref_en)
+          insertion_sizes.append(en-st)
+
     insertion_n = np.sum(insertion_sizes)
 
-    return {
-        'all_insertion_positions': all_insertion_positions,
-        'all_insertion_left_positions': all_insertion_left_positions,
-        'insertion_positions': insertion_positions,
-        'insertion_coordinates': insertion_coordinates,
-        'insertion_sizes': insertion_sizes,
-        'insertion_n': insertion_n,
 
-        'all_deletion_positions': all_deletion_positions,
-        'deletion_positions': deletion_positions,
-        'deletion_coordinates': deletion_coordinates,
-        'deletion_sizes': deletion_sizes,
-        'deletion_n': deletion_n,
+    retDict = {
+	    'all_insertion_positions':all_insertion_positions,
+	    'all_insertion_left_positions':all_insertion_left_positions,
+	    'insertion_positions':insertion_positions,
+	    'insertion_coordinates':insertion_coordinates,
+	    'insertion_sizes':insertion_sizes,
+	    'insertion_n':insertion_n,
 
-        'all_substitution_positions': all_substitution_positions,
-        'substitution_positions': substitution_positions,
-        'all_substitution_values': np.array(all_substitution_values),
-        'substitution_values': np.array(substitution_values),
-        'substitution_n': substitution_n,
+	    'all_deletion_positions':all_deletion_positions,
+	    'deletion_positions':deletion_positions,
+	    'deletion_coordinates':deletion_coordinates,
+	    'deletion_sizes':deletion_sizes,
+	    'deletion_n':deletion_n,
 
-        'ref_positions': ref_positions,
+	    'all_substitution_positions':all_substitution_positions,
+	    'substitution_positions':substitution_positions,
+	    'all_substitution_values':np.array(all_substitution_values),
+	    'substitution_values':np.array(substitution_values),
+	    'substitution_n':substitution_n,
+
+	    'ref_positions':ref_positions,
     }
-
+    return retDict
 
 @cython.boundscheck(False)
 @cython.nonecheck(False)
