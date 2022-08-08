8c93fce9629dd3b15cd5420eb8231fef2a579b73
kclem <k.clement.dev@gmail.com>
2022-08-08 16:04:59 -0400

    Enable processing of cram files by CRISPRessoWGS
    Adds --reference to samtools view when viewing cram files

--- CRISPResso2/CRISPRessoWGSCORE.py
+++ CRISPResso2/CRISPRessoWGSCORE.py
@@ -170,6 +170,18 @@ def get_reference_positions( pos, cigar,full_length=True):
     return positions
 
 def write_trimmed_fastq(in_bam_filename, bpstart, bpend, out_fastq_filename):
+    """ Write the trimmed fastq by extracting reads from a bam, trimming them, and writing them to a file.
+    The bam file was previously filtered for reads at the correct chromosome location.
+
+    Args:
+        in_bam_filename (string): bam input file
+        bpstart (int): start position
+        bpend (int): stop position
+        out_fastq_filename (str): name of file to write reads to
+
+    Returns:
+        n_reasd (int): number of reads written to the output fastq file
+    """    
     p = sb.Popen(
                 'samtools view %s | cut -f1,4,6,10,11' % in_bam_filename,
                 stdout = sb.PIPE,
@@ -215,7 +227,7 @@ def extract_reads(row):
 
         info('Extracting reads in:%s and creating .bam file: %s' % (region, row.bam_file_with_reads_in_region))
 
-        cmd=r'''samtools view -b -F 4 %s %s > %s ''' % (row.original_bam, region, row.bam_file_with_reads_in_region)
+        cmd=r'''samtools view -b -F 4 --reference %s %s %s > %s ''' % (row.reference_file, row.original_bam, region, row.bam_file_with_reads_in_region)
         sb.call(cmd, shell=True)
 
         cmd=r'''samtools index %s ''' % (row.bam_file_with_reads_in_region)
@@ -532,6 +544,7 @@ def main():
         df_regions['bam_file_with_reads_in_region'], df_regions['fastq_file_trimmed_reads_in_region'], df_regions['row_fastq_exists'] = zip(*df_regions.apply(set_filenames, axis=1))
         df_regions['n_reads'] = 0
         df_regions['original_bam'] = args.bam_file #stick this in the df so we can parallelize the analysis and not pass params
+        df_regions['reference_file'] = args.reference_file
 
 
         report_reads_aligned_filename = _jp('REPORT_READS_ALIGNED_TO_SELECTED_REGIONS_WGS.txt')
