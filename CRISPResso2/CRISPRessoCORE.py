aea48b39fa3962106081f78fec7998200a631822
kclem <k.clement.dev@gmail.com>
2025-12-10 16:02:48 -0700

    If crispresso_options_string is set by user, don't force-add any other options (e.g. --disable_adapter_trimming')

--- CRISPResso2/CRISPRessoCORE.py
+++ CRISPResso2/CRISPRessoCORE.py
@@ -3609,10 +3609,13 @@ def main():
             if not args.crispresso_merge:
                 processed_output_filename = _jp('out.extendedFrags.fastq.gz')
                 info('Processing sequences with fastp...')
-                if not args.trim_sequences:
-                    args.fastp_options_string += ' --disable_adapter_trimming --disable_trim_poly_g --disable_quality_filtering --disable_length_filtering'
+                if args.fastp_options_string: # if fastp_options_string has been set by user, just use those options..
+                    pass
                 else:
-                    args.fastp_options_string += ' --detect_adapter_for_pe'
+                    if not args.trim_sequences:
+                        args.fastp_options_string = ' --disable_adapter_trimming --disable_trim_poly_g --disable_quality_filtering --disable_length_filtering'
+                    else:
+                        args.fastp_options_string = ' --detect_adapter_for_pe'
 
                 fastp_cmd = '{command} -i {r1} -I {r2} --merge --merged_out {out_merged} --unpaired1 {unpaired1} --unpaired2 {unpaired2} --overlap_len_require {min_overlap} --thread {num_threads} --json {json_report} --html {html_report} {options} >> {log} 2>&1'.format(
                     command=args.fastp_command,
@@ -3666,10 +3669,13 @@ def main():
                 info('Done!', {'percent_complete': 7})
             else:
                 info('Processing sequences with fastp...')
-                if not args.trim_sequences:
-                    args.fastp_options_string += ' --disable_adapter_trimming --disable_trim_poly_g --disable_quality_filtering --disable_length_filtering'
+                if args.crispresso_options_string: # if crispresso_options_string has been set by user, just use those options..
+                    pass
                 else:
-                    args.fastp_options_string += ' --detect_adapter_for_pe'
+                    if not args.trim_sequences:
+                        args.fastp_options_string = ' --disable_adapter_trimming --disable_trim_poly_g --disable_quality_filtering --disable_length_filtering'
+                    else:
+                        args.fastp_options_string = ' --detect_adapter_for_pe'
 
                 fastp_cmd = '{command} -i {r1} -I {r2} --out1 {unpaired1} --out2 {unpaired2} --thread {num_threads} --json {json_report} --html {html_report} {options} >> {log} 2>&1'.format(
                     command=args.fastp_command,
